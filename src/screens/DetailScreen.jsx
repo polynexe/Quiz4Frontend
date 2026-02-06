688e060c88a6ba18165079e58be2cc085c32fc9b
Pauleen Tabor <pitabor@student.hau.edu.ph>
2026-02-06 20:47:58 +0800

    Implement project details display in DetailScreen; fetch project data using Redux and render project information

--- src/screens/DetailScreen.jsx
+++ src/screens/DetailScreen.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import {Row, Col, Image, ListGroup} from 'react-bootstrap'
+import { listProjects } from '../actions/projectActions';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
 
 function DetailScreen() {
+    const dispatch = useDispatch();
+    const projectList = useSelector((state) => state.projectList);
+    const {loading, error, projects} = projectList;
+    useEffect(() => {
+        dispatch(listProjects());
+    }, []);
   return (
-    <div>DetailScreen</div>
+    <Row>
+        <Col md={6}>
+            <Image src={projects.image} alt='Project Image' fluid />
+        </Col>
+        <Col md={3}>
+            <ListGroup variant='flush'>
+                <ListGroup.Item>
+                    <h3>{projects.name}</h3>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                    <h3>{projects.description}</h3>
+                </ListGroup.Item>
+                <ListGroup.Item>{projects.status}</ListGroup.Item>
+                <ListGroup.Item>{projects.hours_consumed}</ListGroup.Item>
+                <ListGroup.Item>{projects.start_date}</ListGroup.Item>
+                <ListGroup.Item>{projects.end_date}</ListGroup.Item>
+            </ListGroup>
+        </Col>
+    </Row>
   )
 }
 
