82c704613bad272ec88201bfcff8c46c74b46191
Pauleen Tabor <pitabor@student.hau.edu.ph>
2026-02-06 19:23:10 +0800

    Update package dependencies and implement Accordion component in HomeScreen

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import {Row, Col} from 'react-bootstrap';
 
-function HomeScreen() {
+export default function AccordionExpandIcon() {
 return (
-<div>HomeScreen</div>
-)
+<div>
+    <Row className="mb-3">
+        <Col className="text-center">
+             <Accordion>
+                <AccordionSummary expandIcon={<ArrowDropDownIcon />} aria-controls="panel2-content"id="panel2-header">
+                    <Typography component="span">All Projects</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                    malesuada lacus ex, sit amet blandit leo lobortis eget.
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+            <Accordion>
+                <AccordionSummary
+                    expandIcon={<ArrowDropDownIcon />}
+                    aria-controls="panel2-content"
+                    id="panel2-header"
+                >
+                    <Typography component="span">Manager</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                    malesuada lacus ex, sit amet blandit leo lobortis eget.
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+            <Accordion>
+                <AccordionSummary
+                    expandIcon={<ArrowDropDownIcon />}
+                    aria-controls="panel2-content"
+                    id="panel2-header"
+                >
+                    <Typography component="span">Team</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                    malesuada lacus ex, sit amet blandit leo lobortis eget.
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+        </Col>
+    </Row>
+   
+</div>
+);
 }
-
-export default HomeScreen;
